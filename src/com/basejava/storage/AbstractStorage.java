19e0691554b006f0813493775fd796ccb8587e6e
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-04-27 23:48:41 -0400

    Added Logging

--- src/com/basejava/storage/AbstractStorage.java
+++ src/com/basejava/storage/AbstractStorage.java
@@ -6,13 +6,17 @@ import com.basejava.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
     protected static final Comparator<Resume> RESUME_COMPARATOR =
             Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> resumes = doGetAll();
         resumes.sort(RESUME_COMPARATOR);
         return resumes;
@@ -20,39 +24,49 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public final Resume get(String uuid) {
+        LOG.info("get " + uuid);
         SK searchKey = getExistingSearchKey(uuid);
         return doGet(searchKey);
     }
 
     @Override
     public final void update(Resume resume) {
+        LOG.info("update " + resume);
         SK searchKey = getExistingSearchKey(resume.getUuid());
         doUpdate(searchKey, resume);
     }
 
     @Override
     public final void delete(String uuid) {
+        LOG.info("delete " + uuid);
         SK searchKey = getExistingSearchKey(uuid);
         doDelete(searchKey);
     }
 
     @Override
     public final void save(Resume resume) {
+        LOG.info("save " + resume);
         SK searchKey = getNotExistingSearchKey(resume.getUuid());
         doSave(searchKey, resume);
     }
 
     private SK getExistingSearchKey(String uuid) {
+        LOG.info("getExistingSearchKey " + uuid);
         SK searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) {
-            throw new NotExistStorageException(uuid);
+            NotExistStorageException exception = new NotExistStorageException(uuid);
+            LOG.log(Level.WARNING, exception.getMessage(), exception);
+            throw exception;
         }
         return searchKey;
     }
 
     private SK getNotExistingSearchKey(String uuid) {
+        LOG.info("getNotExistingSearchKey " + uuid);
         SK searchKey = getSearchKey(uuid);
         if (isExist(searchKey)) {
+            ExistStorageException exception = new ExistStorageException(uuid);
+            LOG.log(Level.WARNING, exception.getMessage(), exception);
             throw new ExistStorageException(uuid);
         }
         return searchKey;
