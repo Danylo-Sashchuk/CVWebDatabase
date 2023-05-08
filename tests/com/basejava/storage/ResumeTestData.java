0befaf9eb0482c25b068c2f056b9a39ce4ee8c96
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-05-07 23:18:37 -0400

    Refactoring

--- tests/com/basejava/storage/ResumeTestData.java
+++ tests/com/basejava/storage/ResumeTestData.java
@@ -10,97 +10,168 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ResumeTestData {
+    private final MockingData mockingData;
+    private Iterator<String> phonesIterator;
+    private Iterator<String> emailsIterator;
+    private Iterator<String> positionsIterator;
+    private Iterator<String> personalsIterator;
+    private Iterator<String> achievementsIterator;
+    private Iterator<String> qualificationsIterator;
+    private Iterator<String> workTitlesIterator;
+    private Iterator<String> workDescriptionsIterator;
+    private Iterator<String> datesIterator;
+    private Iterator<String> companyNamesIterator;
+    private Iterator<String> companyWebsitesIterator;
+    private Iterator<String> educationNamesIterator;
+    private Iterator<String> educationWebsitesIterator;
+    private Iterator<String> educationTitlesIterator;
+    private Iterator<String> educationDescriptionsIterator;
 
-    private static Iterator<String> phonesIterator = MockingData.getIterator(MockingData.PHONE_NUMBERS);
-    private static Iterator<String> emailsIterator = MockingData.getIterator(MockingData.EMAILS);
-    private static Iterator<String> positionsIterator = MockingData.getIterator(MockingData.POSITIONS);
-    private static Iterator<String> personalsIterator = MockingData.getIterator(MockingData.PERSONALS);
-    private static Iterator<String> achievementsIterator = MockingData.getIterator(MockingData.ACHIEVEMENTS);
-    private static Iterator<String> qualificationsIterator = MockingData.getIterator(MockingData.QUALIFICATIONS);
-    private static Iterator<String> workTitlesIterator = MockingData.getIterator(MockingData.WORK_TITLES);
-    private static Iterator<String> workDescriptionsIterator = MockingData.getIterator(MockingData.WORK_DESCRIPTIONS);
-    private static Iterator<String> datesIterator = MockingData.getIterator(MockingData.DATES);
-    private static Iterator<String> companyNamesIterator = MockingData.getIterator(MockingData.COMPANY_NAMES);
-    private static Iterator<String> companyWebsitesIterator = MockingData.getIterator(MockingData.COMPANY_WEBSITES);
-    private static Iterator<String> educationNamesIterator = MockingData.getIterator(MockingData.EDUCATION_NAMES);
-    private static Iterator<String> educationWebsitesIterator = MockingData.getIterator(MockingData.EDUCATION_WEBSITES);
-    private static Iterator<String> educationTitlesIterator = MockingData.getIterator(MockingData.EDUCATION_TITLE);
-    private static Iterator<String> educationDescriptionsIterator =
-            MockingData.getIterator(MockingData.EDUCATION_DESCRIPTION);
+    public ResumeTestData() {
+        mockingData = MockingData.getInstance();
+        phonesIterator = mockingData.getIterator(mockingData.PHONE_NUMBERS);
+        emailsIterator = mockingData.getIterator(mockingData.EMAILS);
+        positionsIterator = mockingData.getIterator(mockingData.POSITIONS);
+        personalsIterator = mockingData.getIterator(mockingData.PERSONALS);
+        achievementsIterator = mockingData.getIterator(mockingData.ACHIEVEMENTS);
+        qualificationsIterator = mockingData.getIterator(mockingData.QUALIFICATIONS);
+        workTitlesIterator = mockingData.getIterator(mockingData.WORK_TITLES);
+        workDescriptionsIterator = mockingData.getIterator(mockingData.WORK_DESCRIPTIONS);
+        datesIterator = mockingData.getIterator(mockingData.DATES);
+        companyNamesIterator = mockingData.getIterator(mockingData.COMPANY_NAMES);
+        companyWebsitesIterator = mockingData.getIterator(mockingData.COMPANY_WEBSITES);
+        educationNamesIterator = mockingData.getIterator(mockingData.EDUCATION_NAMES);
+        educationWebsitesIterator = mockingData.getIterator(mockingData.EDUCATION_WEBSITES);
+        educationTitlesIterator = mockingData.getIterator(mockingData.EDUCATION_TITLE);
+        educationDescriptionsIterator = mockingData.getIterator(mockingData.EDUCATION_DESCRIPTION);
+    }
 
-    public static Resume createResume(String uuid, String fullName) {
-        if (!phonesIterator.hasNext()) {
+    public Resume createResume(String uuid, String fullName) {
+        if (!hasMoreData()) {
             reloadIterators();
         }
         Resume resume = new Resume(uuid, fullName);
+        addContacts(fullName, resume);
+        addPosition(resume);
+        addPersonal(resume);
+
+        addAchievements(resume);
+        addQualifications(resume);
+
+        addExperiences(resume);
+        addEducations(resume);
+        return resume;
+    }
+
+    private boolean hasMoreData() {
+        return phonesIterator.hasNext();
+    }
+
+    private void reloadIterators() {
+        phonesIterator = mockingData.getIterator(mockingData.PHONE_NUMBERS);
+        emailsIterator = mockingData.getIterator(mockingData.EMAILS);
+        positionsIterator = mockingData.getIterator(mockingData.POSITIONS);
+        personalsIterator = mockingData.getIterator(mockingData.PERSONALS);
+        achievementsIterator = mockingData.getIterator(mockingData.ACHIEVEMENTS);
+        qualificationsIterator = mockingData.getIterator(mockingData.QUALIFICATIONS);
+        workTitlesIterator = mockingData.getIterator(mockingData.WORK_TITLES);
+        workDescriptionsIterator = mockingData.getIterator(mockingData.WORK_DESCRIPTIONS);
+        datesIterator = mockingData.getIterator(mockingData.DATES);
+        companyNamesIterator = mockingData.getIterator(mockingData.COMPANY_NAMES);
+        companyWebsitesIterator = mockingData.getIterator(mockingData.COMPANY_WEBSITES);
+        educationNamesIterator = mockingData.getIterator(mockingData.EDUCATION_NAMES);
+        educationWebsitesIterator = mockingData.getIterator(mockingData.EDUCATION_WEBSITES);
+        educationTitlesIterator = mockingData.getIterator(mockingData.EDUCATION_TITLE);
+        educationDescriptionsIterator = mockingData.getIterator(mockingData.EDUCATION_DESCRIPTION);
+    }
+
+    private void addContacts(String fullName, Resume resume) {
         resume.addContact(ContactType.PHONE_NUMBER, phonesIterator.next());
         resume.addContact(ContactType.EMAIL, generateEmail(fullName));
         resume.addContact(ContactType.GITHUB, generateLink(fullName, "https://github.com/"));
         resume.addContact(ContactType.LINKEDIN, generateLink(fullName, "https://www.linkedin.com/in/"));
         resume.addContact(ContactType.SKYPE, generateSkype(fullName));
-
-        resume.addSection(SectionType.POSITION, new TextSection(positionsIterator.next()));
-        resume.addSection(SectionType.PERSONAL, new TextSection(personalsIterator.next()));
-
-        ListSection achievementsSection = createListSection(achievementsIterator);
-        resume.addSection(SectionType.ACHIEVEMENTS, achievementsSection);
-        ListSection qualificationsSection = createListSection(qualificationsIterator);
-        resume.addSection(SectionType.QUALIFICATIONS, qualificationsSection);
-
-        resume.addSection(SectionType.EXPERIENCE, new CompanySection(createCompaniesList(companyNamesIterator, companyWebsitesIterator, workTitlesIterator, workDescriptionsIterator)));
-        resume.addSection(SectionType.EDUCATION, new CompanySection(createCompaniesList(educationNamesIterator, educationWebsitesIterator, educationTitlesIterator, educationDescriptionsIterator)));
-        return resume;
     }
 
-    private static ListSection createListSection(Iterator<String> iterator) {
-        List<String> list = createComposedList(iterator);
-        return new ListSection(list);
-    }
-
-    public static String generateEmail(String fullName) {
+    private String generateEmail(String fullName) {
         fullName = fullName.replaceAll(" ", "").toLowerCase();
         return fullName + "@" + emailsIterator.next() + ".com";
     }
 
-    public static String generateLink(String fullName, String websiteUrl) {
+    private String generateLink(String fullName, String websiteUrl) {
         fullName = fullName.replaceAll(" ", "").toLowerCase();
         return websiteUrl + fullName;
     }
 
-    public static String generateSkype(String fullName) {
+    private String generateSkype(String fullName) {
         fullName = fullName.replaceAll(" ", ".").toLowerCase();
         return fullName;
     }
 
-    private static List<String> createComposedList(Iterator<String> iterator) {
+    private void addPosition(Resume resume) {
+        resume.addSection(SectionType.POSITION, new TextSection(positionsIterator.next()));
+    }
+
+    private void addPersonal(Resume resume) {
+        resume.addSection(SectionType.PERSONAL, new TextSection(personalsIterator.next()));
+    }
+
+    private void addAchievements(Resume resume) {
+        ListSection achievementsSection = createListSection(achievementsIterator);
+        resume.addSection(SectionType.ACHIEVEMENTS, achievementsSection);
+    }
+
+    private ListSection createListSection(Iterator<String> infoIterator) {
+        List<String> list = createComposedList(infoIterator);
+        return new ListSection(list);
+    }
+
+    private List<String> createComposedList(Iterator<String> infoIterator) {
         List<String> list = new ArrayList<>();
-        list.add(iterator.next());
-        list.add(iterator.next());
-        list.add(iterator.next());
+        list.add(infoIterator.next());
+        list.add(infoIterator.next());
+        list.add(infoIterator.next());
         return list;
     }
 
-    public static List<Company> createCompaniesList(Iterator<String> namesIterator, Iterator<String> websitesIterator, Iterator<String> titlesIterator, Iterator<String> descriptionsIterator) {
+    private void addQualifications(Resume resume) {
+        ListSection qualificationsSection = createListSection(qualificationsIterator);
+        resume.addSection(SectionType.QUALIFICATIONS, qualificationsSection);
+    }
+
+    private void addExperiences(Resume resume) {
+        CompanySection experienceSection = new CompanySection(createCompaniesList(companyNamesIterator,
+                companyWebsitesIterator, workTitlesIterator, workDescriptionsIterator));
+        resume.addSection(SectionType.EXPERIENCE, experienceSection);
+    }
+
+    private void addEducations(Resume resume) {
+        CompanySection educationSection = new CompanySection(createCompaniesList(educationNamesIterator,
+                educationWebsitesIterator, educationTitlesIterator, educationDescriptionsIterator));
+        resume.addSection(SectionType.EDUCATION, educationSection);
+    }
+
+    private List<Company> createCompaniesList(Iterator<String> namesIterator, Iterator<String> websitesIterator,
+                                              Iterator<String> titlesIterator, Iterator<String> descriptionsIterator) {
         List<Company> companies = new ArrayList<>();
-        List<Period> periods = getTwoPeriods(titlesIterator, descriptionsIterator);
+        List<Period> periods = new ArrayList<>();
+        periods.add(createPeriod(titlesIterator, descriptionsIterator));
+        periods.add(createPeriod(titlesIterator, descriptionsIterator));
         companies.add(createCompany(namesIterator, websitesIterator, periods));
-        periods = getTwoPeriods(titlesIterator, descriptionsIterator);
+
+        periods.clear();
+        periods.add(createPeriod(titlesIterator, descriptionsIterator));
+        periods.add(createPeriod(titlesIterator, descriptionsIterator));
         companies.add(createCompany(namesIterator, websitesIterator, periods));
         return companies;
     }
 
-    private static Company createCompany(Iterator<String> namesIterator, Iterator<String> websitesIterator,List<Period> firstPeriodsPair) {
-        return new Company(namesIterator.next(), websitesIterator.next(), firstPeriodsPair);
+    private Company createCompany(Iterator<String> namesIterator, Iterator<String> websitesIterator,
+                                  List<Period> periods) {
+        return new Company(namesIterator.next(), websitesIterator.next(), periods);
     }
 
-    private static List<Period> getTwoPeriods(Iterator<String> titleIterator, Iterator<String> descriptionIterator) {
-        List<Period> periods = new ArrayList<>();
-        periods.add(createPeriod(titleIterator, descriptionIterator));
-        periods.add(createPeriod(titleIterator, descriptionIterator));
-        return periods;
-    }
-
-    private static Period createPeriod(Iterator<String> titleIterator, Iterator<String> descriptionIterator) {
+    private Period createPeriod(Iterator<String> titleIterator, Iterator<String> descriptionIterator) {
         YearMonth start = YearMonth.parse(datesIterator.next());
         YearMonth end = YearMonth.parse(datesIterator.next());
         LocalDate startDate = DateUtil.of(start.getYear(), start.getMonth());
@@ -108,44 +179,5 @@ public class ResumeTestData {
         String workTitle = titleIterator.next();
         String workDescription = descriptionIterator.next();
         return new Period(workTitle, workDescription, startDate, endDate);
-    }
-
-    private static void reloadIterators() {
-        phonesIterator = MockingData.getIterator(MockingData.PHONE_NUMBERS);
-        emailsIterator = MockingData.getIterator(MockingData.EMAILS);
-        positionsIterator = MockingData.getIterator(MockingData.POSITIONS);
-        personalsIterator = MockingData.getIterator(MockingData.PERSONALS);
-        achievementsIterator = MockingData.getIterator(MockingData.ACHIEVEMENTS);
-        qualificationsIterator = MockingData.getIterator(MockingData.QUALIFICATIONS);
-        workTitlesIterator = MockingData.getIterator(MockingData.WORK_TITLES);
-        workDescriptionsIterator = MockingData.getIterator(MockingData.WORK_DESCRIPTIONS);
-        datesIterator = MockingData.getIterator(MockingData.DATES);
-        companyNamesIterator = MockingData.getIterator(MockingData.COMPANY_NAMES);
-        companyWebsitesIterator = MockingData.getIterator(MockingData.COMPANY_WEBSITES);
-        educationNamesIterator = MockingData.getIterator(MockingData.EDUCATION_NAMES);
-        educationWebsitesIterator = MockingData.getIterator(MockingData.EDUCATION_WEBSITES);
-        educationTitlesIterator = MockingData.getIterator(MockingData.EDUCATION_TITLE);
-        educationDescriptionsIterator = MockingData.getIterator(MockingData.EDUCATION_DESCRIPTION);
-    }
-
-    public static void main(String[] args) {
-        final String UUID_1 = "uuid1";
-        final String UUID_2 = "uuid2";
-        final String UUID_3 = "uuid3";
-        final String UUID_4 = "uuid4";
-        final String FULL_NAME_1 = "John Smith";
-        final String FULL_NAME_2 = "Emily Davis";
-        final String FULL_NAME_3 = "Robert Johnson";
-        final String FULL_NAME_4 = "Alice Wilson";
-        System.out.println(createResume(UUID_1, FULL_NAME_1)); //1
-        System.out.println(createResume(UUID_2, FULL_NAME_2));
-        System.out.println(createResume(UUID_3, FULL_NAME_3));//3
-        System.out.println(createResume(UUID_4, FULL_NAME_4));
-        System.out.println(createResume(UUID_4, FULL_NAME_4));//5
-        System.out.println(createResume(UUID_4, FULL_NAME_4));
-        System.out.println(createResume(UUID_4, FULL_NAME_4));//7
-        System.out.println(createResume(UUID_4, FULL_NAME_4));
-        System.out.println(createResume(UUID_4, FULL_NAME_4));//9
-        System.out.println(createResume(UUID_4, FULL_NAME_4));
     }
 }
