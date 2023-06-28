ada682b10444b295b644b94a8d1f6679acc636c5
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-06-27 23:11:04 -0400

    servlet

---
+++ src/com/basejava/web/ResumeServlet.java
@@ -0,0 +1,22 @@
+package com.basejava.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello resumes!" : "Hello " + name + "! ");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+    }
+}
